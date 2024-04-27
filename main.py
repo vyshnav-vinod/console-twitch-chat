f3dc7429e7dd27c23fe1e5bdbe97e34aabb5584b
Vyshnav Vinod <vyshnav.vinod003@gmail.com>
2024-04-27 19:34:52 +0530

    Add chatter color

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 
 import os
 from dotenv import load_dotenv
+from colorist import ColorHex, Effect
 
 from twitchio import Message
 from twitchio.ext import commands
 
-load_dotenv()
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-CHANNELS = os.getenv("CHANNELS").split(",")
 
 class ChatOnConsole(commands.Bot):
     def __init__(self):
@@ -20,11 +18,25 @@ class ChatOnConsole(commands.Bot):
             print(f"Connected to : {channel.name}")
     
     async def event_message(self, message: Message) -> None:
-        print(f"{message.author.name}: {message.content}")
+        chatter_color = message.author.color
+        
+        if chatter_color:
+            chatter_color = ColorHex(chatter_color)
+        else:
+            chatter_color = ColorHex("#FFFFF")
+        
+        print(f"{chatter_color}{Effect.BOLD}{message.author.name}{Effect.BOLD_OFF}{chatter_color.OFF} : {message.content}")
 
 
 if __name__ == "__main__":
+
+    load_dotenv()
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    CHANNELS = os.getenv("CHANNELS").split(",")
+
     obj = ChatOnConsole()
     obj.run()
 
+#TODO: Make this customizable with a json file(Option for timestamps, colors, black and white, notifs or not, etc)
+#TODO: Add option to ignore specific user messages(like bots)
 #IDEA : Maybe send out a notification everytime a twitch chat is received
