7a07c483be5bb8778add6a13af88324542bf4add
Vyshnav Vinod <vyshnav.vinod003@gmail.com>
2024-04-28 11:37:58 +0530

    Load configs from json complete

--- main.py
+++ main.py
@@ -3,41 +3,44 @@
 
 import os
 import json
+import datetime
 from dotenv import load_dotenv
-from colorist import ColorHex, Effect, BgColor
+from colorist import ColorHex, Effect
 
 from twitchio import Message
 from twitchio.ext import commands
 
 
 class ChatOnConsole(commands.Bot):
-    def __init__(self, cfg: dict):
-        self.cfg = cfg
-        super().__init__(token=ACCESS_TOKEN, prefix="", initial_channels=self.cfg.get("CHANNELS", ""))
-    
+    def __init__(self, cfg: dict) -> None:
+        try:
+            self.cfg: dict = cfg
+            super().__init__(token=ACCESS_TOKEN, prefix="", initial_channels=self.cfg.get("CHANNELS", ""))
+        except Exception as e:
+            handle_error(e)
+            
     async def event_ready(self) -> None:
         for channel in self.connected_channels:
             print(f"Connected to : {channel.name}")
     
     async def event_message(self, message: Message) -> None:
-        
-        result_chat: str = ""
+        result_chat: dict = {}
+        result_chat["chatter"] = message.author.name
+        result_chat["content"] = message.content    
 
         if self.cfg.get("SHOW_CHATTER_COLOR"):
             chatter_color: str | None = message.author.color
-            
-            if chatter_color:
-                chatter_color = ColorHex(chatter_color)
-            else:
-                chatter_color = ColorHex("#FFFFF")
-            
-            result_chat = f"{chatter_color}{Effect.BOLD}{message.author.name}{Effect.BOLD_OFF}{chatter_color.OFF} : {message.content}"
+            chatter_color = ColorHex(chatter_color) if chatter_color else ColorHex("#FFFFFF")
+            result_chat["color"] = chatter_color
+            timestamp = message.timestamp if self.cfg.get("SHOW_TIMESTAMP") else ""
         
         else:
-            # Print without colors (Maybe refactor code to only use one single print)
-            result_chat = f"{Effect.BOLD}{message.author.name}{Effect.BOLD_OFF}{Effect.OFF} : {message.content}"
+            result_chat["color"] = ColorHex("#FFFFFF")
+            timestamp = message.timestamp if self.cfg.get("SHOW_TIMESTAMP") else ""
 
-        print(result_chat)
+        result_chat["timestamp"] = timestamp
+        print_chat(result_chat)
+
 
 def get_config(file: str) -> dict:
     try:
@@ -48,15 +51,35 @@ def get_config(file: str) -> dict:
         exit(-1)
 
 
-if __name__ == "__main__":
+def print_chat(result_chat: dict) -> None:
+    timestamp:datetime.datetime | None = ""
+    color: ColorHex = result_chat["color"]
+    chatter: str = result_chat["chatter"]
+    message: str = result_chat["content"]
 
+    if result_chat["timestamp"]:
+        timestamp = result_chat["timestamp"].time().strftime("%H:%M") # It is in UTC
+        print(f"{Effect.BOLD}{color}{chatter} [{timestamp}]{color.OFF}{Effect.BOLD_OFF}{Effect.OFF}: {message}")
+    
+    else:
+        print(f"{Effect.BOLD}{color}{chatter}{color.OFF}{Effect.BOLD_OFF}{Effect.OFF}: {message}")
+
+
+def handle_error(e: Exception):
+    print("Encountered an error : {e}")
+    exit(-1)
+
+
+
+if __name__ == "__main__":
     load_dotenv()
     ACCESS_TOKEN: str = os.getenv("ACCESS_TOKEN")
 
-    obj = ChatOnConsole(get_config("config.json"))
-    obj.run()
-
-#TODO: Make this customizable with a json file(Option for timestamps, colors, black and white, notifs or not, etc)
+    try:
+        obj = ChatOnConsole(get_config("config.json"))
+        obj.run()
+    except Exception as e:
+        handle_error(e)
 #TODO: Highlight when mentioned
 #TODO: Add option to ignore specific user messages(like bots)
 #TODO: Add specific symbols or colors to display whether the user is a mod, broadcaster or the vip
